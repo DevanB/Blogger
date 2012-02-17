aa6e71c94f1392309cc26420ee057be6e9783c8c
Devan Beitel <devan.beitel@gmail.com>
2012-02-17 18:57:21 -0500

    Added more actions, form partial, and flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,33 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(params[:article])
+    @article.save
+    flash[:message] = "You've just created the article '#{@article.title}'!"
+    redirect_to article_path(@article)
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+    flash[:message] = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end  
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy()
+    flash[:message] = "Woah buddy! You just deleted the article '#{@article.title}'!"
+    redirect_to article_path(@article)
+  end
 end
